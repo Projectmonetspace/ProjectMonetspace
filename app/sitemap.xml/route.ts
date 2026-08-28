c76c06d0720c944dd15856eb88ff8b5daa4db744
Projectmonetspace <projectmonetspace@gmail.com>
2026-08-28 13:48:40 +0530

    Add app/sitemap.xml/route.ts

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,27 @@
+import {
+  blogSitemapEntries,
+  latestModification,
+  pagesSitemapEntries,
+  xmlResponse,
+} from "../lib/sitemap-content";
+
+const baseUrl = "https://www.projectmonet.space";
+
+export const revalidate = 3600;
+
+export function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${baseUrl}/pages-sitemap.xml</loc>
+    <lastmod>${latestModification(pagesSitemapEntries)}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/blog-sitemap.xml</loc>
+    <lastmod>${latestModification(blogSitemapEntries)}</lastmod>
+  </sitemap>
+</sitemapindex>
+`;
+
+  return xmlResponse(xml);
+}
